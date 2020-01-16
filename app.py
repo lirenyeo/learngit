82d7f1cf0abde6117182fe2b31a5a5c5556d863f
Liren Yeo <liren90@gmail.com>
2020-01-16 12:21:46 +0800

    added basic login logic

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 def run():
     print('running the app!')
+
+def login():
+    # login logic here
+    print('Sodales ac facilisi etiam lobortis suspendisse nisi augue taciti mollis facilisis, urna pharetra donec ullamcorper dictum blandit porttitor et primis.')
