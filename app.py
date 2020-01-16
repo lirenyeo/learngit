9b441966c5b22b3c90e1fb0030bfd44ef79906cc
Liren Yeo <liren90@gmail.com>
2020-01-16 12:29:39 +0800

    logout logic, 20% done

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ def run():
 def login():
     # login logic here
     print('Sodales ac facilisi etiam lobortis suspendisse nisi augue taciti mollis facilisis, urna pharetra donec ullamcorper dictum blandit porttitor et primis.')
+
+
+def logout():
+    pass
